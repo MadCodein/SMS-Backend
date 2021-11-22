e202e78b26352e4a7fcd091ae8a63644d5d5a04d
Emmanuel Ashitey <eaashitey39@gmail.com>
2021-11-22 19:00:02 +0000

    Wrapped controllers in try catch block so tha I can handle errors

--- controllers/group.js
+++ controllers/group.js
@@ -3,75 +3,95 @@ const makeRequest = require('../service/serve')
 
 // get all groups
 const getAllGroups = async (req, res) => {
-  //  url
-  const url = constants.mnotifyGroupBaseUrl + '?key=' + constants.SECRET_KEY
+  try {
+    //  url
+    const url = constants.mnotifyGroupBaseUrl + '?key=' + constants.SECRET_KEY
 
-  const response = await makeRequest('get', {}, url)
+    const response = await makeRequest('get', {}, url)
 
-  return res.json(response.data)
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 // get individual groups
 const getGroupID = async (req, res) => {
-  //  url
-  const url =
-    constants.mnotifyGroupBaseUrl +
-    '/' +
-    req.params.id +
-    '?key=' +
-    constants.SECRET_KEY
+  try {
+    //  url
+    const url =
+      constants.mnotifyGroupBaseUrl +
+      '/' +
+      req.params.id +
+      '?key=' +
+      constants.SECRET_KEY
 
-  const response = await makeRequest('get', {}, url)
+    const response = await makeRequest('get', {}, url)
 
-  return res.json(response.data)
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 // create new group
 const createGroup = async (req, res, next) => {
-  //  url
-  const url = constants.mnotifyGroupBaseUrl + '?key=' + constants.SECRET_KEY
+  try {
+    //  url
+    const url = constants.mnotifyGroupBaseUrl + '?key=' + constants.SECRET_KEY
 
-  // check if the request body is not empty
-  if (Object.keys(req.body).length === 0) {
-    return next(new Error('All Fields are required'))
+    // check if the request body is not empty
+    if (Object.keys(req.body).length === 0) {
+      return next(new Error('All Fields are required'))
+    }
+
+    const response = await makeRequest('post', req.body, url)
+
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
   }
-
-  const response = await makeRequest('post', req.body, url)
-
-  return res.json(response.data)
 }
 
 // update individual groups
 const updateGroupID = async (req, res, next) => {
-  const url =
-    constants.mnotifyGroupBaseUrl +
-    '/' +
-    req.params.id +
-    '?key=' +
-    constants.SECRET_KEY
+  try {
+    const url =
+      constants.mnotifyGroupBaseUrl +
+      '/' +
+      req.params.id +
+      '?key=' +
+      constants.SECRET_KEY
 
-  // check if the request body is not empty
-  if (Object.keys(req.body).length === 0) {
-    return next(new Error('All Fields are required'))
+    // check if the request body is not empty
+    if (Object.keys(req.body).length === 0) {
+      return next(new Error('All Fields are required'))
+    }
+
+    const response = await makeRequest('put', req.body, url)
+
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
   }
-
-  const response = await makeRequest('put', req.body, url)
-
-  return res.json(response.data)
 }
 
 // delete group
 const deleteGroup = async (req, res, next) => {
-  const url =
-    constants.mnotifyGroupBaseUrl +
-    '/' +
-    req.params.id +
-    '?key=' +
-    constants.SECRET_KEY
+  try {
+    const url =
+      constants.mnotifyGroupBaseUrl +
+      '/' +
+      req.params.id +
+      '?key=' +
+      constants.SECRET_KEY
 
-  const response = await makeRequest('delete', {}, url)
+    const response = await makeRequest('delete', {}, url)
 
-  return res.json(response.data)
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 module.exports = {
--- controllers/template.js
+++ controllers/template.js
@@ -4,76 +4,98 @@ const makeRequest = require('../service/serve')
 
 // get all message templates
 const getAllMessageTemplates = async (req, res) => {
-  //  url
-  const url = constants.mnotifyTemplateBaseUrl + '?key=' + constants.SECRET_KEY
+  try {
+    //  url
+    const url =
+      constants.mnotifyTemplateBaseUrl + '?key=' + constants.SECRET_KEY
 
-  const response = await makeRequest('get', {}, url)
+    const response = await makeRequest('get', {}, url)
 
-  return res.json(response.data)
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 // get individual message templates
 const getMessageTemplate = async (req, res) => {
-  //  url
-  const url =
-    constants.mnotifyTemplateBaseUrl +
-    '/' +
-    req.params.id +
-    '?key=' +
-    constants.SECRET_KEY
+  try {
+    //  url
+    const url =
+      constants.mnotifyTemplateBaseUrl +
+      '/' +
+      req.params.id +
+      '?key=' +
+      constants.SECRET_KEY
 
-  const response = await makeRequest('get', {}, url)
+    const response = await makeRequest('get', {}, url)
 
-  return res.json(response.data)
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 // create new message template
 const createMessageTemplate = async (req, res, next) => {
-  //  url
-  const url = constants.mnotifyTemplateBaseUrl + '?key=' + constants.SECRET_KEY
+  try {
+    //  url
+    const url =
+      constants.mnotifyTemplateBaseUrl + '?key=' + constants.SECRET_KEY
 
-  // check if the request body is not empty
-  if (Object.keys(req.body).length === 0) {
-    return next(new Error('All Fields are required'))
+    // check if the request body is not empty
+    if (Object.keys(req.body).length === 0) {
+      return next(Error('All Fields are required'))
+    }
+
+    const response = await makeRequest('post', req.body, url)
+
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
   }
-
-  const response = await makeRequest('post', req.body, url)
-
-  return res.json(response.data)
 }
 
 // update individual message templates
 const updateTemplateID = async (req, res, next) => {
-  const url =
-    constants.constants.BASE_URL +
-    '/' +
-    req.params.id +
-    '?key=' +
-    constants.SECRET_KEY
+  try {
+    const url =
+      constants.constants.BASE_URL +
+      '/' +
+      req.params.id +
+      '?key=' +
+      constants.SECRET_KEY
 
-  // check if the request body is not empty
-  if (Object.keys(req.body).length === 0) {
-    return next(new Error('All Fields are required'))
+    // check if the request body is not empty
+    if (Object.keys(req.body).length === 0) {
+      return next(new Error('All Fields are required'))
+    }
+
+    const response = await makeRequest('put', req.body, url)
+
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
   }
-
-  const response = await makeRequest('put', req.body, url)
-
-  return res.json(response.data)
 }
 
 // delete individual message templates
 const deleteTemplate = async (req, res, next) => {
-  //  url
-  const url =
-    constants.mnotifyTemplateBaseUrl +
-    '/' +
-    req.params.id +
-    '?key=' +
-    constants.SECRET_KEY
+  try {
+    //  url
+    const url =
+      constants.mnotifyTemplateBaseUrl +
+      '/' +
+      req.params.id +
+      '?key=' +
+      constants.SECRET_KEY
 
-  const response = await makeRequest('delete', {}, url)
+    const response = await makeRequest('delete', {}, url)
 
-  return res.json(response.data)
+    return res.json(response.data)
+  } catch (error) {
+    return next(error)
+  }
 }
 
 module.exports = {
